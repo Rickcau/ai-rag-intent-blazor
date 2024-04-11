09ea0261778b331fb40a54e539ce8ae2e9e3efd3
Rick Caudle <rickcau@hotmail.com>
2024-04-11 17:06:09 -0400

    add MudBlazor

--- blazor/Program.cs
+++ blazor/Program.cs
@@ -1,6 +1,7 @@
 using BlazorChat;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>
@@ -8,5 +9,6 @@ builder.RootComponents.Add<App>
 
 builder.Services.AddScoped(sp =>
  new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
